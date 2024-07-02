9d9b7497732007058276e00bcbed0b467209317e
Nolen Johnson <johnsonnolen@gmail.com>
2024-07-02 14:04:59 -0400

    berlin: berlin_global-user 13 T1RGS33.135-109-9-22 779029-4d7c6d release-key

--- Android.mk
+++ Android.mk
@@ -6,6 +6,6 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),berlin)
 
-$(call add-radio-file-sha1-checked,radio/bluetooth.img,6d78b055c3b6e601022d4fbf2b5174ba70a5e80b)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,02b627ad6e363ac4232247c2119d489a4dbee881)
 
 endif
